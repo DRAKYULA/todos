add8a51d0ec0157fa1ccd91395c777f94bd7259d
drakyula <shicks@kalkomey.com>
2018-06-19 12:03:35 -0500

    allow a user to mark a todo as incomplete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'todos#index'
   resources :todos do
-    resource :completion, only: :create
+    resource :completion, only: [:create, :destroy]
   end
   resource  :session, only: [:new, :create]
 end
