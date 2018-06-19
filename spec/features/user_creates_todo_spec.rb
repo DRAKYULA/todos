29764f119311506138af6b83cd32c464388924d0
drakyula <shicks@kalkomey.com>
2018-06-19 09:18:29 -0500

    User can 'sign in'

--- spec/features/user_creates_todo_spec.rb
+++ spec/features/user_creates_todo_spec.rb
@@ -3,7 +3,7 @@ require 'capybara/rspec'
 
 feature 'User creates todo' do
   scenario 'successfully' do
-    visit root_path
+    sign_in
 
     click_on 'Add a new todo'
     fill_in 'Title', with: 'Buy milk'
